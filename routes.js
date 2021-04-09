44015487dc64a3b3fbba859ef4d5b794b358b5bf
hrushikesh jadhav <rushi611@gmail.com>
2021-04-09 16:42:51 +0530

    structure updated

---
+++ routes.js
@@ -0,0 +1,10 @@
+const routes = require('next-routes')(); //return a function and () means execute it
+
+routes
+    .add('/campaigns/new', '/campaigns/new')
+    .add('/campaigns/:address', '/campaigns/show')
+    .add('/campaigns/:address/requests', '/campaigns/requests/index')
+    .add('/campaigns/:address/requests/new', '/campaigns/requests/new')
+    .add('about');
+
+module.exports = routes;
