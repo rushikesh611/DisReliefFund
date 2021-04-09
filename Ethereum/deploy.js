b0c3080e0be88a6a79095ce6ae2bfb7f32566590
hrushikesh jadhav <rushi611@gmail.com>
2021-04-09 20:16:27 +0530

    blockchain connection

---
+++ Ethereum/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+require('dotenv').config();
+
+// const provider = new HDWalletProvider(
+//     process.env.mnemonic,
+//     process.env.link
+// );
+const provider = new HDWalletProvider("","HTTP://127.0.0.1:7545");
+
+const web3 = new Web3(provider);
+const deploy = async () => {
+    // const accounts = '0x9a7C80b94bc013C3861ad4fcf35A12159F5C581c';
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attemping to deploy to accounts ', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+        .deploy({ data: '0x' + compiledFactory.bytecode })
+        .send({ from: accounts[0] });
+
+    console.log('Contract deploy to ', result.options.address);
+};
+
+deploy();
